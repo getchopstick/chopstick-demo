355a2441b0904e1714df7571b330339849316825
Jannick Vandaele <jannickvandaele@hotmail.com>
2015-10-16 16:12:10 +0200

    added option to set custom class

--- js/_components/_toggle.js
+++ js/_components/_toggle.js
@@ -4,6 +4,7 @@ chopstick.toggle =
     {
         showHideToggle: '.js-show-hide',
         targetDataAtr: 'target-selector',
+        classDataAtr: 'toggle-class',
         defaultTriggerClass: 'is-toggled',
         defaultTargetClass: 'is-hidden'
     },
@@ -22,18 +23,23 @@ chopstick.toggle =
         // Bind show hide event
         $(this.settings.showHideToggle).on('touchstart click', function(e){
             var trigger = $(this);
+            var target = trigger.data(module.settings.targetDataAtr);
+            var toggleClass = trigger.data(module.settings.classDataAtr);
+            console.log(toggleClass);
             // Check if action needs to be prevented
             if (trigger.data("action") == "none") {
                 e.preventDefault();
             }
-            module.showHide(trigger.data(module.settings.targetDataAtr));
+            
+            module.showHide(target, toggleClass);
             trigger.toggleClass(module.settings.defaultTriggerClass);
         });
     },
 
-    showHide: function(targets)
+    showHide: function(targets, toggleClass)
     {
+        if (typeof toggleClass === 'undefined') { toggleClass = this.settings.defaultTargetClass; }
         //  Toggle the 'is-hidden' class
-        $(targets).toggleClass(this.settings.defaultTargetClass);
+        $(targets).toggleClass(toggleClass);
     }
 };
